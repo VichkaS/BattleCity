79b049ad1800057a40d851215c721cae9f2ee047
VichkaS <vi4ka.vs@gmail.com>
2015-05-22 19:39:17 +0300

    Добавлен класс CTankInfo

---
+++ BattleCity.NET/BattleCity.NET/CTankInfo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace BattleCity.NET
+{
+    class CTankInfo
+    {
+        struct TankStatistics
+        {
+            int shotsFired;
+            int successfulShots;
+            int destroyedTanks;
+            int collectedMedicineChest;
+            int collectedAntibonus;
+            int lostHealth;
+            int distanceTraveled;
+        }
+        private string m_dllPath;
+        private Color m_color;
+        
+        public CTankInfo(string dll, Color tankColor)
+        {
+            m_dllPath = dll; 
+            m_color = tankColor; 
+        }
+
+        public string DLLPath 
+        {
+            get
+            {
+                return m_dllPath;
+            }
+        }
+
+        public Color Color
+        {
+            get
+            {
+                return m_color;
+            }
+        }
+        public TankStatistics Statistics;
+    }
+}
--- BattleCity.NET/BattleCity.NET/FResults.Designer.cs
+++ BattleCity.NET/BattleCity.NET/FResults.Designer.cs
@@ -28,26 +28,26 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.bSave = new System.Windows.Forms.Button();
-			this.bExit = new System.Windows.Forms.Button();
-			this.bRestart = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// listView1
-			// 
-			this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bSave = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.bRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3,
@@ -56,95 +56,103 @@
             this.columnHeader6,
             this.columnHeader7,
             this.columnHeader8});
-			this.listView1.GridLines = true;
-			this.listView1.Location = new System.Drawing.Point(12, 136);
-			this.listView1.Name = "listView1";
-			this.listView1.ShowGroups = false;
-			this.listView1.Size = new System.Drawing.Size(533, 196);
-			this.listView1.TabIndex = 0;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
-			// 
-			// columnHeader1
-			// 
-			this.columnHeader1.Text = "ФИО";
-			// 
-			// columnHeader2
-			// 
-			this.columnHeader2.Text = "количество выстрелов";
-			// 
-			// columnHeader3
-			// 
-			this.columnHeader3.Text = "% успешных выстрелов";
-			// 
-			// columnHeader4
-			// 
-			this.columnHeader4.Text = "танков уничтожено";
-			// 
-			// columnHeader5
-			// 
-			this.columnHeader5.Text = "аптечек собрано";
-			// 
-			// columnHeader6
-			// 
-			this.columnHeader6.Text = "антибонусов собрано";
-			// 
-			// columnHeader7
-			// 
-			this.columnHeader7.Text = " здоровья потеряно";
-			// 
-			// columnHeader8
-			// 
-			this.columnHeader8.Text = "пройденное расстояние";
-			// 
-			// bSave
-			// 
-			this.bSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-			this.bSave.Location = new System.Drawing.Point(12, 338);
-			this.bSave.Name = "bSave";
-			this.bSave.Size = new System.Drawing.Size(136, 23);
-			this.bSave.TabIndex = 1;
-			this.bSave.Text = "Save results";
-			this.bSave.UseVisualStyleBackColor = true;
-			// 
-			// bExit
-			// 
-			this.bExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.bExit.Location = new System.Drawing.Point(470, 338);
-			this.bExit.Name = "bExit";
-			this.bExit.Size = new System.Drawing.Size(75, 23);
-			this.bExit.TabIndex = 2;
-			this.bExit.Text = "Exit";
-			this.bExit.UseVisualStyleBackColor = true;
-			this.bExit.Click += new System.EventHandler(this.bExit_Click);
-			// 
-			// bRestart
-			// 
-			this.bRestart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.bRestart.Location = new System.Drawing.Point(389, 338);
-			this.bRestart.Name = "bRestart";
-			this.bRestart.Size = new System.Drawing.Size(75, 23);
-			this.bRestart.TabIndex = 3;
-			this.bRestart.Text = "New game";
-			this.bRestart.UseVisualStyleBackColor = true;
-			// 
-			// FResults
-			// 
-			this.AcceptButton = this.bSave;
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.CancelButton = this.bExit;
-			this.ClientSize = new System.Drawing.Size(557, 373);
-			this.Controls.Add(this.bRestart);
-			this.Controls.Add(this.bExit);
-			this.Controls.Add(this.bSave);
-			this.Controls.Add(this.listView1);
-			this.MinimumSize = new System.Drawing.Size(350, 250);
-			this.Name = "FResults";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "Game results";
-			this.ResumeLayout(false);
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(16, 167);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(709, 240);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ФИО";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "количество выстрелов";
+            this.columnHeader2.Width = 165;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "% успешных выстрелов";
+            this.columnHeader3.Width = 166;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "танков уничтожено";
+            this.columnHeader4.Width = 149;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "аптечек собрано";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "антибонусов собрано";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = " здоровья потеряно";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "пройденное расстояние";
+            // 
+            // bSave
+            // 
+            this.bSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bSave.Location = new System.Drawing.Point(16, 416);
+            this.bSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(181, 28);
+            this.bSave.TabIndex = 1;
+            this.bSave.Text = "Save results";
+            this.bSave.UseVisualStyleBackColor = true;
+            // 
+            // bExit
+            // 
+            this.bExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.bExit.Location = new System.Drawing.Point(627, 416);
+            this.bExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(100, 28);
+            this.bExit.TabIndex = 2;
+            this.bExit.Text = "Exit";
+            this.bExit.UseVisualStyleBackColor = true;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
+            // 
+            // bRestart
+            // 
+            this.bRestart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bRestart.Location = new System.Drawing.Point(519, 416);
+            this.bRestart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bRestart.Name = "bRestart";
+            this.bRestart.Size = new System.Drawing.Size(100, 28);
+            this.bRestart.TabIndex = 3;
+            this.bRestart.Text = "New game";
+            this.bRestart.UseVisualStyleBackColor = true;
+            // 
+            // FResults
+            // 
+            this.AcceptButton = this.bSave;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.bExit;
+            this.ClientSize = new System.Drawing.Size(743, 459);
+            this.Controls.Add(this.bRestart);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(461, 297);
+            this.Name = "FResults";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Game results";
+            this.ResumeLayout(false);
 
 		}
 
